d7345d97e5bca478437f38b8eb2136258a92062c
Xingyao Wang <xingyao6@illinois.edu>
2024-10-14 20:20:03 +0000

    make docker image lower case to comply with docker

--- evaluation/swe_bench/run_infer.py
+++ evaluation/swe_bench/run_infer.py
@@ -100,7 +100,7 @@ def get_instance_docker_image(instance_id: str) -> str:
     image_name = image_name.replace(
         '__', '_s_'
     )  # to comply with docker image naming convention
-    return DOCKER_IMAGE_PREFIX.rstrip('/') + '/' + image_name
+    return (DOCKER_IMAGE_PREFIX.rstrip('/') + '/' + image_name).lower()
 
 
 def get_config(
@@ -225,6 +225,7 @@ def initialize_runtime(
         logger.info(obs, extra={'msg_type': 'OBSERVATION'})
         if isinstance(obs, ErrorObservation):
             logger.error(f'Failed to source ~/.bashrc: {obs.content}')
+
         assert_and_raise(
             obs.exit_code == 0, f'Failed to source ~/.bashrc: {obs.content}'
         )
